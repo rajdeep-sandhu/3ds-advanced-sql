c2426336f94f2d68c65dc54b4b914a65137985c7
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-31 20:37:39 +0000

    create 01_window_functions.py

---
+++ src/3ds_advanced_sql/01_window_functions.py
@@ -0,0 +1,13 @@
+import marimo
+
+__generated_with = "0.21.1"
+app = marimo.App(width="full", app_title="01_window_functions")
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
