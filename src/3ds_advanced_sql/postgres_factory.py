6b4b0aeafdb9af3d524f4152469ad2a4425b5276
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-04-03 23:54:00 +0000

    create postgres_factory.py

---
+++ src/3ds_advanced_sql/postgres_factory.py
@@ -0,0 +1,40 @@
+import os
+
+from sqlalchemy import URL, create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
+class PostgresFactory:
+    """Concrete postgres factory."""
+
+    def __init__(self) -> None:
+        self._username = os.environ["POSTGRES_USER"]
+        self._password = os.environ["POSTGRES_PASSWORD"]
+        self._database = os.environ["POSTGRES_DB"]
+        self._host = os.environ.get("POSTGRES_HOST", "localhost")
+        self._port = 5432
+
+        # postgresql to use psycopg2, posgresql+psycopg to use psycopg3
+        url = URL.create(
+            drivername="postgresql+psycopg",
+            username=self._username,
+            password=self._password,
+            host=self._host,
+            port=self._port,
+            database=self._database,
+        )
+
+        self._engine = create_engine(url)
+
+        self._sessionmaker = sessionmaker(
+            bind=self._engine, autoflush=False, autocommit=False
+        )
+
+    def create_engine(self) -> Engine:
+        """Create an Engine instance."""
+        return self._engine
+
+    def create_session(self) -> Session:
+        """Create a session."""
+        return self._sessionmaker()
