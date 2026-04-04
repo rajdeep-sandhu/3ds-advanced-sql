7e2549bf86a2aa74a918593e665e4c4e5dc1c890
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-04-04 11:33:56 +0000

    add create_database()

--- src/advanced_sql/01_window_functions.py
+++ src/advanced_sql/01_window_functions.py
@@ -8,6 +8,9 @@ with app.setup:
     import os
     import psycopg
     import sqlalchemy
+
+    from pathlib import Path
+    from sqlalchemy import Engine
     from advanced_sql.postgres_factory import PostgresFactory
 
 
@@ -29,36 +32,61 @@ def _():
 
 @app.cell
 def _():
-    return
-
-
-@app.cell
-def _(Engine):
     # Connect to postgres
     factory: PostgresFactory = PostgresFactory()
     engine: Engine = factory.create_engine()
     return (engine,)
 
 
-@app.cell
-def _(engine: "Engine"):
+@app.cell(hide_code=True)
+def _():
     _df = mo.sql(
         f"""
         SELECT VERSION();
-        """,
-        engine=engine
+        """
     )
     return
 
 
-@app.cell
-def _(engine: "Engine"):
+@app.cell(hide_code=True)
+def _(engine: Engine):
     _df = mo.sql(
         f"""
         SELECT * FROM information_schema.tables;
         """,
         engine=engine
     )
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ### Create database from SQL file
+    """)
+    return
+
+
+@app.function
+def create_database(sql_file: Path, engine: Engine) -> None:
+    """Create database using the supplied SQL file"""
+    with open(sql_file, "r") as f:
+        sql: str = f.read()
+
+    with engine.raw_connection() as raw_conn:
+        # with raw_conn.cursor() as cur:
+        #     cur.execute(sql)
+        # raw_conn.commit()
+        cur = raw_conn.cursor()
+        cur.execute(sql)
+        cur.close()
+        raw_conn.commit()
+
+
+@app.cell
+def _(engine: Engine):
+    sql_file: Path = Path(__file__).parent / "employees.sql"
+    create_database(sql_file=sql_file, engine=engine)
     return
 
 
