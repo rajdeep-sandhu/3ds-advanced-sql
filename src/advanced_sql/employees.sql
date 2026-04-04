d4531c02ba4f8309ffbf01553a1d4c50c5ddfa04
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-04-04 22:37:30 +0000

    amend gender ENUM for TABLE employees

--- src/advanced_sql/employees.sql
+++ src/advanced_sql/employees.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0260e5e532ef9eaee0eb7f3684b39ac20e3d46bfe1bac5706a7ad1bee7a30a39
-size 93402398
+oid sha256:e8448f23ca3842e21c571adb692105bf5b957cc2a6f444b539a5cfa983300a83
+size 93402406
