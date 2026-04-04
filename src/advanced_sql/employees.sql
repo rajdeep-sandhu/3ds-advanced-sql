4442b4cb0946ee0502939b3606629a528ee543ce
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-04-04 17:57:24 +0000

    amend create database statement

--- src/advanced_sql/employees.sql
+++ src/advanced_sql/employees.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0575ef7129d00383cb42e6748eb5466f81534bbfcb9559f64eb5bd756bd1d7c9
-size 93402545
+oid sha256:e00bff6e9a7383dad171d8414a71e6b3280b79f2cbc97a5d0592c037a51b0f02
+size 93402531
