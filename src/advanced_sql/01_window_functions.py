d0d6142d43f6632d94f55a026512328950976b7e
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-04-05 15:54:17 +0000

    add reset_database()

--- src/advanced_sql/01_window_functions.py
+++ src/advanced_sql/01_window_functions.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.21.1"
+__generated_with = "0.22.4"
 app = marimo.App(width="full", app_title="01_window_functions")
 
 with app.setup:
@@ -67,25 +67,50 @@ def _():
     return
 
 
-@app.function
-def create_database(sql_file: Path, engine: Engine) -> None:
-    """Create database using the supplied SQL file"""
-    with open(sql_file, "r") as f:
-        sql: str = f.read()
+@app.cell
+def _():
+    def reset_database() -> None:
+        """Reset the employee database."""
+        user = os.environ["POSTGRES_USER"]
+        password = os.environ["POSTGRES_PASSWORD"]
+        database = os.environ["POSTGRES_DB"]
+        host = os.environ.get("POSTGRES_HOST", "localhost")
+        port = 5432
 
-    with engine.raw_connection() as raw_conn:
-        # with raw_conn.cursor() as cur:
-        #     cur.execute(sql)
-        # raw_conn.commit()
-        cur = raw_conn.cursor()
-        cur.execute(sql)
+        conn = psycopg.connect(
+            host=host, user=user, password=password, port=port, autocommit=True
+        )
+    
+        cur = conn.cursor()
+        cur.execute("DROP DATABASE IF EXISTS employees;")
+        cur.execute("CREATE DATABASE employees;")
         cur.close()
-        raw_conn.commit()
+        conn.close()
+
+        print("'employees' database created.")
+
+
+    def create_database(sql_file: Path, engine: Engine) -> None:
+        """Create database using the supplied SQL file"""
+        with open(sql_file, "r") as f:
+            sql: str = f.read()
+
+        with engine.raw_connection() as raw_conn:
+            # with raw_conn.cursor() as cur:
+            #     cur.execute(sql)
+            # raw_conn.commit()
+            cur = raw_conn.cursor()
+            cur.execute(sql)
+            cur.close()
+            raw_conn.commit()
+
+    return create_database, reset_database
 
 
 @app.cell
-def _(engine: Engine):
+def _(create_database, engine: Engine, reset_database):
     sql_file: Path = Path(__file__).parent / "employees.sql"
+    reset_database()
     create_database(sql_file=sql_file, engine=engine)
     return
 
