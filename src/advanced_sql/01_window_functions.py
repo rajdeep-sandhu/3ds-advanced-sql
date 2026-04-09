d5d6cb8c83705c9224626e93f3ea2549a843f2af
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-04-09 23:08:35 +0000

    add query to list `Staff` employees with salaries in ascending order, with row numbers assigned in ascending and descending order.

--- src/advanced_sql/01_window_functions.py
+++ src/advanced_sql/01_window_functions.py
@@ -465,12 +465,42 @@ def _(engine: Engine, salaries, titles):
     return
 
 
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ### List `Staff` employees with salaries in ascending order, with row numbers assigned in ascending and descending order.
+
+    - Retrieve the employee number (`emp_no`) and job title (`title`) from the `titles` table, and the salary (`salary`) from the salaries table.
+    - Add a fourth column, named `row_num1`, starting from `1` and incrementing by 1 for each row for every employee from the obtained result.
+    - Also, add a fifth column, named `row_num2`, which provides the opposite values, starting from the total number of records obtained for a given employee and continuing down to 1.
+    - Include only data about `'Staff'` members and employees with a number no greater than `10006`.
+    - Order the result by `emp_no`, `salary`, and `row_num1` in ascending order.
+    """)
+    return
+
+
 @app.cell
-def _(engine: Engine, titles):
+def _(engine: Engine, salaries, titles):
     _df = mo.sql(
         f"""
-        SELECT * FROM titles
-        WHERE emp_no = 10004
+        SELECT
+            t.emp_no,
+            t.title,
+            s.salary,
+            ROW_NUMBER() OVER(PARTITION BY t.emp_no ORDER BY s.salary ASC) AS row_num1,
+            ROW_NUMBER() OVER(PARTITION BY t.emp_no ORDER BY s.salary DESC) AS row_num2
+        FROM
+        	titles t
+        	INNER JOIN
+        	salaries s
+        		ON t.emp_no = s.emp_no
+        WHERE
+        	t.emp_no <= 10006
+        	AND t.title = 'Staff'
+        ORDER BY
+        	t.emp_no,
+        	s.salary,
+        	row_num1;
         """,
         engine=engine
     )
