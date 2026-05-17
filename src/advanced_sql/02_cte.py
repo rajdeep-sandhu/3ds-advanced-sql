381511d358aeb19c8204a105de9e266cd350ce14
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-05-17 10:17:11 +0000

    add setup cell with imports.

--- src/advanced_sql/02_cte.py
+++ src/advanced_sql/02_cte.py
@@ -3,9 +3,22 @@ import marimo
 __generated_with = "0.22.4"
 app = marimo.App(width="full", app_title="02_cte")
 
+with app.setup:
+    import marimo as mo
+    import os
+    import psycopg
+    import sqlalchemy
 
-@app.cell
+    from pathlib import Path
+    from sqlalchemy import Engine, Connection
+    from advanced_sql.postgres_factory import PostgresFactory
+
+
+@app.cell(hide_code=True)
 def _():
+    mo.md(r"""
+    # 02. Common Table Expressions (CTE)
+    """)
     return
 
 
