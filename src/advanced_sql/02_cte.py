cace456355adc7c8b8abdc419d959a2b0a848b8b
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-05-17 10:24:22 +0000

    add database creation logic

--- src/advanced_sql/02_cte.py
+++ src/advanced_sql/02_cte.py
@@ -50,6 +50,50 @@ def _(engine: Engine):
     return
 
 
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ### Create database from SQL file
+    """)
+    return
+
+
+@app.cell
+def _():
+    def reset_schema(engine: Engine) -> None:
+        """
+        Resets the public schema to a clean state.
+        Equivalent to recreating the database for the purpose of this tutorial.
+        """
+        with engine.begin() as conn:
+            conn.exec_driver_sql("DROP SCHEMA public CASCADE;")
+            conn.exec_driver_sql("CREATE SCHEMA public;")
+
+    def create_database(sql_file: Path, engine: Engine) -> None:
+        """Create database using the supplied SQL file"""
+        with open(sql_file, "r") as f:
+            sql: str = f.read()
+
+        with engine.raw_connection() as raw_conn:
+            # with raw_conn.cursor() as cur:
+            #     cur.execute(sql)
+            # raw_conn.commit()
+            cur = raw_conn.cursor()
+            cur.execute(sql)
+            cur.close()
+            raw_conn.commit()
+
+    return create_database, reset_schema
+
+
+@app.cell
+def _(create_database, engine: Engine, reset_schema):
+    sql_file: Path = Path(__file__).parent / "employees.sql"
+    reset_schema(engine=engine)
+    create_database(sql_file=sql_file, engine=engine)
+    return
+
+
 @app.cell
 def _(engine: Engine):
     _df = mo.sql(
