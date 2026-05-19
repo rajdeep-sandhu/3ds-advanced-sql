2eb14d1757c317f83ea01a3db74f9ab7613992e6
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-05-19 21:35:33 +0000

    add query to get the number of male employees whose highest contract salary values were lower than the company average, using SUM().

--- src/advanced_sql/02_cte.py
+++ src/advanced_sql/02_cte.py
@@ -607,5 +607,70 @@ def _(employees, engine: Engine, salaries):
     return
 
 
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ### Get the number of male employees whose highest contract salary values were lower than the company average.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Using two CTE and a `SUM()` function in the SELECT statement.
+    """)
+    return
+
+
+@app.cell
+def _(employees, engine: Engine, salaries):
+    _df = mo.sql(
+        f"""
+        WITH
+            avg_salary AS
+            (
+            SELECT
+            	AVG(salary) AS avg_salary
+            FROM
+            	salaries
+            ),
+
+        	max_m_salaries AS
+        	(
+            SELECT
+                e.emp_no,
+                MAX(s.salary) AS max_salary
+            FROM
+            	employees e
+            	INNER JOIN
+            	salaries s
+            		ON e.emp_no = s.emp_no
+            			AND e.gender = 'M'
+            GROUP BY
+            	e.emp_no
+            )
+
+        SELECT
+        	SUM(
+            	CASE
+            		WHEN max_m_salaries.max_salary < avg_salary.avg_salary
+            		THEN 1 ELSE 0
+            	END
+            ) AS m_highest_salaries_above_avg,
+            COUNT(e.emp_no) AS total_m_highest_salaries
+        FROM
+            employees e
+            INNER JOIN
+        	max_m_salaries
+            	ON e.emp_no = max_m_salaries.emp_no
+        	CROSS JOIN
+        	avg_salary;
+        """,
+        engine=engine
+    )
+    return
+
+
 if __name__ == "__main__":
     app.run()
