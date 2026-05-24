642911ed85560feb00f875f441ddbc93fe1389b0
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-05-24 07:05:21 +0000

    add app width and title

---
+++ src/advanced_sql/03_temporary_tables.py
@@ -0,0 +1,13 @@
+import marimo
+
+__generated_with = "0.22.4"
+app = marimo.App(width="full", app_title="03_temporary_tables")
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
