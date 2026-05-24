4faa664abbc08dbe0f797b35205eedf4a5996694
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-05-24 09:18:32 +0000

    add setup cell with imports

--- src/advanced_sql/03_temporary_tables.py
+++ src/advanced_sql/03_temporary_tables.py
@@ -3,10 +3,15 @@ import marimo
 __generated_with = "0.22.4"
 app = marimo.App(width="full", app_title="03_temporary_tables")
 
+with app.setup:
+    import marimo as mo
+    import os
+    import psycopg
+    import sqlalchemy
 
-@app.cell
-def _():
-    return
+    from pathlib import Path
+    from sqlalchemy import Engine, Connection
+    from advanced_sql.postgres_factory import PostgresFactory
 
 
 if __name__ == "__main__":
